c96f6e7be70ae904f712cf53b82c140a747896a6
angelo-wq <software.developer.angelo@gmail.com>
2026-02-10 14:45:16 -0500

    Creación del generador de contraseñas.

---
+++ app.py
@@ -0,0 +1,28 @@
+import random
+import string
+import os
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def generar_password(longitud):
+     caracteres = string.ascii_letters + string.digits + string.punctuation
+     password = "".join(random.choice(caracteres) for i in range(longitud))
+     return password
+
+while True:
+     try:
+          print("=============================")
+          print(Style.BRIGHT + Fore.CYAN + """
+          Ctrl + C para salir
+          """)
+          size = int(input("Logitud deseada para la contraseña: >_ "))
+          print('Cargando...')
+          os.system('cls' if os.name == 'nt' else 'clear')
+          print(" ")
+          print(Fore.GREEN + "---PASSWORD GENERATOR---")
+          print(" ")
+          print(Fore.GREEN + f"Tu nueva contraseña es: {generar_password(size)}")
+     except KeyboardInterrupt:
+          print(Fore.RED + "\nSaliendo...")
+          break
